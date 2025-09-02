d8a8a9705953d1d8449634fb5a7e82731f2b99e6
Danté Kellman-Thompson <dante.kellman@icloud.com>
2025-09-02 21:38:17 +0100

    added to the code.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,20 +31,12 @@ export default function Header() {
 
 The code needs to get hold of the users inputted text and the language selected to then pass on to the AI, 
 when the translate button is hit.
-The fields then need to either be replaced or the button links to a page to display the following:
 
-Original text - with the users text in a text field 
-Your translation - with the translated users text depending on the language they selected.
-Start over button - takes the user back to the original screen. This will need to be tracked with useState.
-
-Make sure CSS follows best practice and looks decent on larger screens.
-
-Potentially might have to track if the text has been translated with useState and if there has been any change with the 
-selected radios by using an onChange and useState if I encounter issues with the ai knowing what has been selected.
-
-Mapping the radios into the prompts so the AI knows what language it needs to translate ot.
+Mapping the radios into the prompts so the AI knows what language it needs to translate. isLangauge state will be important here as it holds the language the user has selected.
 
 Will create a frontend helper 
 Serverless function will be created and linked to netflify to protect the API key.
+
+Make sure CSS follows best practice and looks decent on larger screens.
 
 */
